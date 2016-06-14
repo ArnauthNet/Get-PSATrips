4024de34917811e16745ec61d50184ab9a874fab
Luis Arnauth <luisarnauth@outlook.com>
2016-06-14 17:29:23 +0100

    First version

---
+++ Get-PSATrips.ps1
@@ -0,0 +1,26 @@
+﻿Function Get-DateFromEpoch {
+	param (
+		[Parameter(Mandatory=$true)]
+		$unixTime
+	)
+	$epoch = New-Object -Type DateTime -ArgumentList 1970, 1, 1, 0, 0, 0, 0
+	Write-output $epoch.AddSeconds($unixTime)
+}
+
+$trips = Get-Content ".\my_psa.trips" | ConvertFrom-Json
+
+$myTrips = New-Object System.Collections.Generic.List[System.Object]
+
+
+foreach ($trip in $trips.trips) {
+    $currentTrip = [pscustomobject]@{
+        tripDate = Get-DateFromEpoch -unixTime $trip.dte
+        tripDuration = $trip.tme
+        tripGasConsumption = $trip.vol
+        tripDistante = $trip.dst/1000
+        tripGasAverage = ($trip.vol*100) / ($trip.dst/1000)
+    }
+    $myTrips.Add($currentTrip)
+}
+
+$myTrips | select -last 1
